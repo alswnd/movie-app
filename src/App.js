6a9aad0cf4b801e712c3ce2347f85c34cb6067e4
alswnd <alswnd0427@gmail.com>
2020-06-29 21:09:11 +0900

    add styles

--- src/App.js
+++ src/App.js
@@ -28,22 +28,31 @@ class App extends React.Component {
 
   render() {
     const { isLoading, movies } = this.state;
-
     return (
       <section className="container">
-        {isLoading ?
+        {isLoading ? (
           <div className="loader">
-            <span className="loader_text">Loading...</span>
-          </div> : (
-            <div className="movies" >
-              {movies.map(movie => {
-                return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />
-              })}
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
             </div>
           )}
       </section>
     );
   }
+
 }
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -5,16 +5,18 @@ import './Movie.css';
 function Movie({ year, title, summary, poster, genres }) {
     return (
         <div className="movie">
-            <img src={poster} alt={title} title={title}></img>
-
-            <div className="movie-data">
-                <h3 className="movie-title" style={{ backgroundColor: 'red' }}>{title}</h3>
-                <h5 className="movie-year">{year}</h5>
-                <p className="movie-summary">{summary}</p>
-                <ul className="genres">{genres.map((genre, index) => {
-                    return <li key={index} className="genre">{genre}</li>
-                })}
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
+                    ))}
                 </ul>
+                <p className="movie__summary">{summary}</p>
             </div>
         </div>
     );
