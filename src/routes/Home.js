11bf5f70436dceaceca631f4f77520ad7dfe7051
alswnd <alswnd0427@gmail.com>
2020-06-29 21:29:35 +0900

    App.js -> Home.js

---
+++ src/routes/Home.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './Home.css';
+import axios from 'axios';
+import Movie from '../components/Movie';
+
+const URL_SORT_BY_RATING = 'https://yts-proxy.now.sh/list_movies.json?sort_by=rating';
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(URL_SORT_BY_RATING);
+    this.setState({ isLoading: false, movies });
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div>
+          )}
+      </section>
+    );
+  }
+
+}
+
+export default Home;
